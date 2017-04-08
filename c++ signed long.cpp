fb38d6f8cf66316e8e878ed67f79884f3fee339f
Stev01 <sa362001@gmail.com>
2017-04-08 10:48:53 +0200

    programmi c++ Adel, Sacchetto
    programmi c++

---
+++ c++ signed long.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   signed  int v;
+   v = 2147483647 ;
+   cout << " la variabile e" <<  v << endl;
+   return 0;
+}
---
+++ c++ usigned long.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   usigned long v;
+   v = 2147483647 ;
+   cout << " la variabile e" <<  v << endl;
+   return 0;
+}
